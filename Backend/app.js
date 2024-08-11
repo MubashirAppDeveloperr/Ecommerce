cb35ca9e493a2baabf25a2418188cfdb7527d8e0
Mubashir Ahmad <niazi9999mubashir@gmail.com>
2024-08-11 20:01:01 +0500

    second commit

--- Backend/app.js
+++ Backend/app.js
@@ -28,7 +28,7 @@ app.use(
       credentials: true,
     },
     {
-      origin: ["https://ecommerce-frontend-kappa-pied.vercel.app"],
+      origin: ["https://ecommerce-frontend-eight-pearl.vercel.app"],
       methods: ["POST", "GET"],
       credentials: true,
     }
